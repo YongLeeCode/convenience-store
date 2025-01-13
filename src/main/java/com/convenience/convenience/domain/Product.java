09212a1a2d66238fcbcd3db97f000fbe7feb57a0
YongLeeCode <yonghwan311@gmail.com>
2025-01-13 18:01:37 +0900

    feat: product 클래스 생성

--- src/main/java/com/convenience/convenience/domain/Product.java
+++ src/main/java/com/convenience/convenience/domain/Product.java
@@ -1,4 +1,25 @@
 package com.convenience.convenience.domain;
 
 public class Product {
+
+    private String name;
+    private Integer price;
+    private Integer quantity;
+    private String promotion;
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public String getPromotion() {
+        return promotion;
+    }
 }
