e35170ed1ce6ebb9aa8b427f1bffdf649b6f05e6
YongLeeCode <yonghwan311@gmail.com>
2025-01-13 18:02:07 +0900

    feat: promotion class 생성

--- src/main/java/com/convenience/convenience/domain/Promotion.java
+++ src/main/java/com/convenience/convenience/domain/Promotion.java
@@ -1,4 +1,9 @@
 package com.convenience.convenience.domain;
 
 public class Promotion {
+    private String promotionName;
+    private Integer buy;
+    private Integer get;
+    private String start_date;
+    private String end_date;
 }
