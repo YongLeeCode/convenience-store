06282ed711b3eb0978db574d2df64e428f4cf28e
YongLeeCode <yonghwan311@gmail.com>
2025-01-14 17:06:24 +0900

    feat: 터미널에서 확인할 수 있는 출력 구현

--- src/main/java/com/convenience/convenience/ConvenienceApplication.java
+++ src/main/java/com/convenience/convenience/ConvenienceApplication.java
@@ -1,13 +1,45 @@
 package com.convenience.convenience;
 
+import com.convenience.convenience.controller.ProductController;
+import com.convenience.convenience.repository.ListProductRepository;
+import com.convenience.convenience.service.ProductService;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration;
+import org.modelmapper.config.Configuration.AccessLevel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ConvenienceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConvenienceApplication.class, args);
+
+		//		ListProductRepository productRepository = new ListProductRepository();
+		//		ProductService productService = new ProductService(productRepository, new ModelMapper());
+		//		ProductController productController = new ProductController(productService);
+
+		ApplicationContext context = SpringApplication.run(ConvenienceApplication.class, args);
+		ProductController productController = context.getBean(ProductController.class);
+
+
+		productController.findProducts(null).forEach(product -> {
+			System.out.printf("Name: %s, Price: %d, Promotion: %s, Quantity: %d%n",
+					product.getName(), product.getPrice(),
+					product.getPromotion(), product.getQuantity());
+		});
+
+		//		SpringApplication.run(ConvenienceApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration()
+				.setFieldAccessLevel(AccessLevel.PRIVATE)
+				.setFieldMatchingEnabled(true);
+		return modelMapper;
 	}
 
 }
