a7a869ee3375b976690c550fd7da26810df3be7e
YongLeeCode <yonghwan311@gmail.com>
2025-01-16 23:07:27 +0900

    refactor: product, promotion의 개터 포함한 메서드 추가

--- src/main/java/com/convenience/convenience/domain/Product.java
+++ src/main/java/com/convenience/convenience/domain/Product.java
@@ -1,11 +1,17 @@
 package com.convenience.convenience.domain;
 
+import java.util.Objects;
+
 public class Product {
 
     private String name;
     private Integer price;
     private String promotion;
     private Integer quantity;
+
+    public Integer getPrice() {
+        return price;
+    }
 
     public Product(String name, int price, String promotion, int quantity) {
         this.name = name;
@@ -14,7 +20,48 @@ public class Product {
         this.quantity = quantity;
     }
 
+    public String getPromotion() {
+        return promotion;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
     public Boolean containsName(String name) {
         return this.name.contains(name);
+    }
+
+    public Boolean containsPromotion(String promotion) {
+        if (this.promotion == null || promotion == null) {
+            return false;
+        }
+        if (this.quantity <= 0) {
+            return false;
+        }
+        return this.promotion.contains(promotion);
+    }
+
+    public int decreaseQuantity(Integer quantity) {
+        if (this.quantity > quantity) {
+            this.quantity -= quantity;
+            return 0;
+        } else {
+            int remain = quantity - this.quantity;
+            this.quantity = 0;
+            return remain;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Product product = (Product) o;
+        return Objects.equals(name, product.name);
     }
 }
--- src/main/java/com/convenience/convenience/domain/Promotion.java
+++ src/main/java/com/convenience/convenience/domain/Promotion.java
@@ -1,9 +1,35 @@
 package com.convenience.convenience.domain;
 
+import java.time.LocalDate;
+
 public class Promotion {
+
     private String promotionName;
     private Integer buy;
     private Integer get;
-    private String start_date;
-    private String end_date;
+    private LocalDate start_date;
+    private LocalDate end_date;
+
+    public Promotion(
+            String promotionName,
+            Integer buy,
+            Integer get,
+            LocalDate start_date,
+            LocalDate end_date
+    ) {
+        this.promotionName = promotionName;
+        this.buy = buy;
+        this.get = get;
+        this.start_date = start_date;
+        this.end_date = end_date;
+    }
+
+    public String getPromotionName() {
+        return promotionName;
+    }
+
+    public boolean isApplicable(String promotion) {
+        this.promotionName.equals(promotion);
+        return true;
+    }
 }
