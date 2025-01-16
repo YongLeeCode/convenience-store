a946a7b915c38e448a3e211db5e84c3d87b4d871
YongLeeCode <yonghwan311@gmail.com>
2025-01-16 23:12:03 +0900

    feat: 프로모션 레포지토리 구현

--- src/main/java/com/convenience/convenience/repository/promotion/ListPromotionRepository.java
+++ src/main/java/com/convenience/convenience/repository/promotion/ListPromotionRepository.java
@@ -1,4 +1,21 @@
 package com.convenience.convenience.repository.promotion;
 
-public class ListPromotionRepository {
+import com.convenience.convenience.domain.Promotion;
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ListPromotionRepository implements PromotionRepository {
+    private static final List<Promotion> PROMOTIONS = List.of(
+            new Promotion("탄산2+1", 2, 1, LocalDate.of(2024, 1, 1), LocalDate.of(2024, 12, 31)),
+            new Promotion("MD추천상품", 1, 1, LocalDate.of(2024, 1, 1), LocalDate.of(2024, 12, 31)),
+            new Promotion("반짝할인", 1, 1, LocalDate.of(2024, 11, 1), LocalDate.of(2024, 11, 30))
+    );
+
+    public List<Promotion> findByPromotionName(String name) {
+        return PROMOTIONS.stream()
+                .filter(promotion -> promotion.getPromotionName().equals(name))
+                .toList();
+    }
 }
--- src/main/java/com/convenience/convenience/repository/promotion/PromotionRepository.java
+++ src/main/java/com/convenience/convenience/repository/promotion/PromotionRepository.java
@@ -1,4 +1,8 @@
 package com.convenience.convenience.repository.promotion;
 
+import com.convenience.convenience.domain.Promotion;
+import java.util.List;
+
 public interface PromotionRepository {
+    List<Promotion> findByPromotionName(String name);
 }
